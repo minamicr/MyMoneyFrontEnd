c36b79617d5c48b18e3ddbb8723b17a16214c498
minamicr <minamicr@yahoo.com.br>
2018-03-07 18:56:46 -0300

    Add files via upload

---
+++ src/main/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    //sempre que for referenciar css utilizar className, pois a palavra 
+    //class no react é reservada para classes; desta forma não temos conflito no react
+    <div className='wrapper'>
+        <h1>App</h1>
+    </div>
+)
